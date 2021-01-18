0b7dfd930d223ba7837257789f723b01322f23f4
Nicolas Hoquet <nicolas.hoquet@gmail.com>
2021-01-18 22:37:39 +0200

    Update connect.php

--- vendor/connect/connect.php
+++ vendor/connect/connect.php
@@ -5,33 +5,27 @@
 *@Author: Nicolas HOQUET
 **/
 
-namespace Vendor\Connect;
+namespace Vendor;
 
 class Connect
 {
-	private static $conn = null;
-	
-	public static function conn()
+	public function conn()
 	{
 		$servername = 'localhost:3306';
 		$username = 'root';
 		$password = '';
 		$dbname = 'product_db';
-		
-		if (is_null(self::$conn))
-		{
-			self::$conn = new \mysqli($servername, $username, $password, $dbname);
+
+		$conn = new \mysqli($servername, $username, $password, $dbname);
+
+		if ($conn->connect_error) {
+			die("Connection failed: ".$conn->connect_error);
 		}
 
-		if (self::$conn->connect_error) {
-			die("Connection failed: ".self::$conn->connect_error);
-		}
-		
 		#set the enconding to utf-8
-		mysqli_query(self::$conn, "SET NAMES 'utf8'");
+		mysqli_query($conn, "SET NAMES 'utf8'");
+		return $conn;
 		
-		return self::$conn;
 	}
-	
 }
 // EOF
