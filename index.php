65a533db89ec42537e081b296d2cf77c4a59b2bc
Nicolas Hoquet <nicolas.hoquet@gmail.com>
2021-01-18 20:53:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
+
+/**
+*@package: Scandiweb assignment
+*@Author: Nicolas HOQUET
+**/
+
 session_start();
+
 require 'vendor/autoloader.php';
 require 'models/autoloader.php';
 $display = new Router($_SERVER['REQUEST_URI']);
 $display->route();
+
 //EOF
